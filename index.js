5dba1efab5a68e6ce286027362da7ae405fd903c
faiton <faithon6109@naver.com>
2020-07-03 19:20:15 +0900

    adding middlewares

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-hello dd
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = 4000;
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`));
