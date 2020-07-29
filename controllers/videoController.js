7fe197f1b3192a6f8cda69fa1c3eedfea3c15dad
faiton <faithon6109@naver.com>
2020-07-29 18:32:17 +0900

    building views, controllers, db ...

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,12 +1,25 @@
-export const home = (req, res) => res.render("home", {pageTitle: "home"});
+import routes from "../routes";
+
+export const home = (req, res) => {
+    res.render("home", {pageTitle: "home", videos});
+}
+
 export const search = (req, res) => {
     const {
         query: { term : searchingBy }
     } = req;
-    res.render("search", {pageTitle: "search", searchingBy});
+    res.render("search", {pageTitle: "search", searchingBy, videos});
 }
-export const videos = (req, res) => res.render("videos", {pageTitle: "videos"});
-export const upload = (req, res) => res.render("upload", {pageTitle: "upload"});
+
+export const getUpload = (req, res) => res.render("upload", {pageTitle: "upload"});
+export const postUpload = (req, res) => {
+    const {
+        body: {file, title, description}
+    } = req;
+    // To Do: Upload and Save
+    res.redirect(routes.videoDetail(12223));
+}
+
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: "videoDetail"});
 export const editVideo = (req, res) => res.render("editVideo", {pageTitle: "editVideo"});
 export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle: "deleteVideo"});
