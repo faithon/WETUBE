2fcad0f1fffe380a368e739e263c8f184e2c60cb
faiton <faithon6109@naver.com>
2020-08-04 17:23:22 +0900

    setting webpack

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(helmet());
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,5 +24,5 @@ app.use(localsMiddleware, localsMiddleware);
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
- 
+
 export default app;
